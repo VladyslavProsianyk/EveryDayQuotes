ff5ff6db3eb899176dc0d1db851301c1c37f9e23
vlapro <vladyslav.prosianyk@gmail.com>
2022-07-02 16:29:57 +1200

    Initial Commit

---
+++ EveryDayQuotes/EveryDayQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EveryDayQuotesApp.swift
+//  EveryDayQuotes
+//
+//  Created by Vladyslav Prosianyk on 02.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct EveryDayQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
